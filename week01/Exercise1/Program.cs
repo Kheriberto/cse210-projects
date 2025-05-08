7fdd45d6f4ba94aa7e4ceabbdc189d8e6c081ae8
kheriberto <pp.kherig@gmail.com>
2025-05-07 21:38:15 -0700

    Completed task 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        string fnme = "";
+        string lnme = "";
+        
+        Console.Write("What is your first name? ");
+        fnme = Console.ReadLine();
+        
+        Console.Write("What is your last name? ");
+        lnme = Console.ReadLine();
+        
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {lnme}, {fnme} {lnme}");
     }
-}
+} 
