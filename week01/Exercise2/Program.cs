3bc6cd566a7d8e42358de5f24191538da068f10c
kheriberto <pp.kherig@gmail.com>
2025-05-08 01:31:05 -0700

    Completed exercise 2 and 3.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,67 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        int score;
+        int residualScore;
+        string grade;
+        string gradeSign;
+        string gradeLetterSign;
+
+        Console.Write("What is the score? ");
+        score = int.Parse(Console.ReadLine());
+
+        if (score >= 90)
+            {
+                grade = "A";
+            }
+        else if (score < 90 && score >= 80)
+            {
+                grade = "B";
+            }
+        else if (score < 80 && score >= 70)
+            {
+                grade = "C";
+            }
+        else if (score < 70 && score >= 60)
+            {
+                grade = "D";
+            }
+        else
+            {
+                grade = "F";
+            }
+
+        if (grade != "F")
+            {
+                residualScore = score % 10;
+
+                if (grade == "A" && score >= 97)
+                    {
+                        gradeSign = "";
+                    }
+                else if (residualScore >= 7)
+                    {
+                        gradeSign = "+";
+                    }
+                else if (residualScore >= 3 && residualScore < 7)
+                    {
+                        gradeSign = "-";
+                    }
+                else
+                    {
+                        gradeSign = "";
+                    }
+            }
+        else
+            {
+                gradeSign = "";
+            }
+
+        gradeLetterSign = grade + gradeSign;
+        
+        Console.WriteLine($"The letter grade for score {score} is: {gradeLetterSign}");
+        Console.WriteLine();
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,67 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        int magicNumber;
+        int guess;
+        int attempts = 0;
+        string outcome;
+        string response = "yes";
+        bool compare;
+        List<int> usedNumbers = new List<int>();
+
+        Random randomGenerator = new Random();
+        magicNumber = randomGenerator.Next(1,100);
+        
+        do
+        {   
+            Console.Write("Guess the magic number: ");
+            guess = int.Parse(Console.ReadLine());
+
+            compare = guess == magicNumber;
+
+            if (!compare)
+                {
+                    if (guess > magicNumber)
+                        {
+                            outcome = "Lower";
+                        }
+                    else
+                        {
+                            outcome = "Higher";
+                        }
+                    
+                    if (!usedNumbers.Contains(guess))
+                        {
+                            usedNumbers.Add(guess);
+                        }
+                    else
+                        {
+                            Console.WriteLine($"You have tried {guess} before.");
+                        }
+                    attempts += 1;
+                    Console.WriteLine($"{outcome}");
+                }
+            else
+                {
+                    attempts += 1;
+                    Console.WriteLine();
+                    Console.WriteLine($"You've guessed it! The magic number is {guess}!");
+                    if (attempts == 1)
+                        {
+                            Console.WriteLine($"It took you {attempts} attempt");
+                        }
+                    else
+                        {
+                            Console.WriteLine($"It took you {attempts} attempts");
+                        }
+                    magicNumber = randomGenerator.Next(1,100);
+                    Console.Write("\nDo you want to play again? ");
+                    response = Console.ReadLine();
+                    Console.WriteLine();
+                    usedNumbers.Clear();
+                    attempts = 0;
+                }
+        } while (response == "yes");
+        Console.Write("Thank you for playing this game!\n");
     }
 }
